65d0c778d6ebe811d1a3316670202c5f9c0cb878
Toni Lähdekorpi <toni@lygon.net>
2012-01-13 16:56:32 +0200

    Howto

--- howto.php
+++ howto.php
@@ -2,4 +2,4 @@
 // This is a HOWTO:
 require_once("wiky.inc.php"); // Include the library (obviously)
 $wiky=new wiky; // Create a new wiky to any variable You'd like. Could be $mooming
-echo $wiky->parse(file_get_contents("input.wiki")); // call for the function parse() on the variable You created and pass some unparsed text to it, it will return parsed HTML or false if the content was empty. In this example we are loading the file input.wiki and passing it's contents
+echo $wiky->parse(file_get_contents("input.wiki")); // Call for the function parse() on the variable You created and pass some unparsed text to it, it will return parsed HTML or false if the content was empty. In this example we are loading the file input.wiki and passing it's contents
