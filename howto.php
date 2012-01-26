3c567312670f211bec5d0fdc85609051b2dd9585
Toni Lähdekorpi <toni@lygon.net>
2012-01-26 10:46:49 +0200

    Added htmlspecialchars() to the example

--- howto.php
+++ howto.php
@@ -8,5 +8,7 @@ require_once("wiky.inc.php");
 // If you pass true to __construct (new wiky(true)), "S" PCRE modifier will be added to all regular expressions. This gives a performance boost when running parse thousands of times. Extreme usage only.
 $wiky=new wiky;
 
-// Call for the function parse() on the variable You created and pass some unparsed text to it, it will return parsed HTML or false if the content was empty. In this example we are loading the file input.wiki and passing it's contents
-echo $wiky->parse(file_get_contents("input.wiki"));
+// Call for the function parse() on the variable You created and pass some unparsed text to it, it will return parsed HTML or false if the content was empty. In this example we are loading the file input.wiki, escaping all html characters with htmlspecialchars and running parse and echoing the output
+$input=file_get_contents("input.wiki");
+$input=htmlspecialchars($input);
+echo $wiky->parse($input);
