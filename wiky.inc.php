86149e67fb1c9a94526782a87b65b4da95cb379b
Sylvain Lévesque <slevesque@slevesque-Serval-Professional.gezere.com>
2012-07-25 14:08:36 -0400

    * Fixed use case when ordered or unordered list start on the first line.

--- wiky.inc.php
+++ wiky.inc.php
@@ -38,13 +38,13 @@ class wiky {
 			"/^:: *(.+)$/m",						// Subindentation second pass
 	
 			// Ordered list
-			"/[\n\r]#.+([\n|\r]#.+)+/",					// First pass, finding all blocks
+			"/[\n\r]?#.+([\n|\r]#.+)+/",					// First pass, finding all blocks
 			"/[\n\r]#(?!#) *(.+)(([\n\r]#{2,}.+)+)/",			// List item with sub items of 2 or more
 			"/[\n\r]#{2}(?!#) *(.+)(([\n\r]#{3,}.+)+)/",			// List item with sub items of 3 or more
 			"/[\n\r]#{3}(?!#) *(.+)(([\n\r]#{4,}.+)+)/",			// List item with sub items of 4 or more
 	
 			// Unordered list
-			"/[\n\r]\*.+([\n|\r]\*.+)+/",					// First pass, finding all blocks
+			"/[\n\r]?\*.+([\n|\r]\*.+)+/",					// First pass, finding all blocks
 			"/[\n\r]\*(?!\*) *(.+)(([\n\r]\*{2,}.+)+)/",			// List item with sub items of 2 or more
 			"/[\n\r]\*{2}(?!\*) *(.+)(([\n\r]\*{3,}.+)+)/",			// List item with sub items of 3 or more
 			"/[\n\r]\*{3}(?!\*) *(.+)(([\n\r]\*{4,}.+)+)/",			// List item with sub items of 4 or more
@@ -80,13 +80,13 @@ class wiky {
 			"<dd>$1</dd>",
 	
 			// Ordered list
-			"\n<ol>$0\n</ol>",
+			"\n<ol>\n$0\n</ol>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 			"\n<li>$1\n<ol>$2\n</ol>\n</li>",
 	
 			// Unordered list
-			"\n<ul>$0\n</ul>",
+			"\n<ul>\n$0\n</ul>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
 			"\n<li>$1\n<ul>$2\n</ul>\n</li>",
